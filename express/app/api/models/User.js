a4a56508181310303b51b07b1ef644bab62ecb1e
JamesIrwin3 <139516801+JamesIrwin3@users.noreply.github.com>
2024-07-18 11:48:43 -0400

    #9 - Set up the basic user model. see #10 for more information.

---
+++ express/app/api/models/User.js
@@ -0,0 +1,60 @@
+import { hash } from "bcrypt";
+import { model, Schema } from "mongoose";
+
+
+const UserSchema = new Schema({
+	username: {
+		type: String,
+		unique: true,
+		require: [true, "A username must be provided"],
+	},
+	email: {
+		type: String,
+		unique: true,
+		require: [true, "A valid fullsail email must be provided"],
+		validate: { 
+			validator: v=> !!v && /^[a-zA-Z0-9._%+-]*@([a-zA-Z0-9-]+\.)*fullsail\.edu$/.test(v),
+			message: ({value}) => `${value} is not a valid fullsail email.`
+		}
+	},
+	password: {
+		type: String,
+		require: [true, "A password must be provided"]
+	},
+	firstName: {
+		type: String,
+		require: [true, "A first name must be provided"]
+	},
+	lastName: {
+		type: String,
+		require: [true, "A last name must be provided"]
+	},
+	role: {
+		type: String,
+		enum: {
+			values: ["admin", "moderator", "user"],
+			message: `{VALUE} is not supported`
+		},
+		default: "user"
+	},
+	joinDate: {
+		type: Date,
+		default: new Date()
+	}
+});
+
+const SALT_ROUNDS = 15; 
+
+/**
+ * Hash the password.
+ */
+UserSchema.pre('save', async function(){
+	if(this.isModified('password')){
+		const password = await hash(this.password, SALT_ROUNDS);
+		this.password = password;
+	}
+});
+
+const User = model("User", UserSchema);
+
+export default User;
