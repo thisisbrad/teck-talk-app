121befd6417f938d7f45ed934978303e76443932
Brad Beltowski <bradbeltowskigmail.com>
2024-07-25 16:03:26 -0400

    Adding directory to eslint command

--- express/eslint.config.js
+++ express/eslint.config.js
@@ -2,9 +2,17 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-  {
-    files: ["**/*.js"],
-    languageOptions: { globals: globals.node },
-  },
+  { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": [
+        "error",
+        { vars: "all", args: "after-used", ignoreRestSiblings: false },
+      ],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-undef": "error",
+    },
+  },
 ];
